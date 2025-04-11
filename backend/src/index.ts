46f03777fc7504bdd65eb81c5d268f86cace8c30
Martin John M <johnmmartin74@gmail.com>
2025-04-11 07:30:37 +0200

    added order router with types

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,9 +12,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use("api/v1/order", orderRouter)
-app.use("api/v1/tickers", tickersRouter)
-app.use("api/v1/depth", depthRouter)
+app.use("/api/v1/order", orderRouter)
+app.use("/api/v1/tickers", tickersRouter)
+app.use("/api/v1/depth", depthRouter)
 
 app.listen(3001, () => {
     console.log("Server is running on 3001");
--- backend/src/routes/order.ts
+++ backend/src/routes/order.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { RedisManager } from "../RedisManager";
-import { CANCEL_ORDER, CREATE_ORDER, OrderCancelledMessageFromOrderBook, OrderPlacedMessageFromOrderBook } from "../types";
-import { CancelOrderMessageToEngine, CreateOrderMessageToEngine } from "../types/to";
+import { CANCEL_ORDER, CREATE_ORDER, GET_OPEN_ORDERS, OpenOrdersMessageFromOrderBook, OrderCancelledMessageFromOrderBook, OrderPlacedMessageFromOrderBook } from "../types";
+import { CancelOrderMessageToEngine, CreateOrderMessageToEngine, GetOpenOrdersMessageToEngine } from "../types/to";
 
 export const orderRouter = Router()
 
 orderRouter.post("/", async (req, res) => {
     const { market, price, quantity, side, userId } = req.body;
+
+    
     console.log({ market, price, quantity, side, userId });
     const response = await RedisManager.getInstance().sendAndAwait<CreateOrderMessageToEngine, OrderPlacedMessageFromOrderBook>({
         type: CREATE_ORDER,
@@ -18,8 +20,9 @@ orderRouter.post("/", async (req, res) => {
             userId
         }
     })
-    res.json(response.payload)
     
+    res.json(response.payload)
+    return
 })
 
 orderRouter.delete("/", async (req, res) => {
@@ -31,5 +34,15 @@ orderRouter.delete("/", async (req, res) => {
             market
         }
     })
-    
+    res.json(response.payload)
 })
+orderRouter.get("/open", async (req, res) => {
+    const response = await RedisManager.getInstance().sendAndAwait<GetOpenOrdersMessageToEngine, OpenOrdersMessageFromOrderBook>({
+        type: GET_OPEN_ORDERS,
+        data: {
+            userId: req.query.userId as string,
+            market: req.query.market as string
+        }
+    });
+    res.json(response.payload);
+});
